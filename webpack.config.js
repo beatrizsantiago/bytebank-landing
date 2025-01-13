0bf6a2ad93c7e9479694dffd551069b184dee833
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-01-13 11:34:54 -0300

    🔧 remove dotenv and related environment variable configuration from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,7 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
-const Dotenv = require("dotenv-webpack");
-const webpack = require('webpack');
-const dotenv = require("dotenv");
 
 module.exports = (webpackConfigEnv, argv) => {
-  const isProduction = argv.mode === "production";
-
-  const envConfig = dotenv.config({
-    path: isProduction ? "./.env.production" : "./.env.development",
-  }).parsed;
-
-  // Mapeia as variáveis para o DefinePlugin
-  const envKeys = Object.keys(envConfig || {}).reduce((prev, key) => {
-    prev[`process.env.${key}`] = JSON.stringify(envConfig[key]);
-    return prev;
-  }, {});
-
   const defaultConfig = singleSpaDefaults({
     orgName: "bytebank",
     projectName: "landing",
@@ -36,8 +21,5 @@ module.exports = (webpackConfigEnv, argv) => {
         },
       ],
     },
-    plugins: [
-      new webpack.DefinePlugin(envKeys),
-    ],
   });
 };
