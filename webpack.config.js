a3773fa9c9a2bda286998094f7c081da4a12d5d1
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-01-13 18:47:07 -0300

    🔧 add dotenv-webpack plugin to manage environment variables in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const { merge } = require("webpack-merge");
+const Dotenv = require('dotenv-webpack');
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 
 module.exports = (webpackConfigEnv, argv) => {
@@ -9,10 +10,17 @@ module.exports = (webpackConfigEnv, argv) => {
     argv,
   });
 
+  const envFilePath = argv.mode === "production" ? ".env.production" : ".env.development";
+
   return merge(defaultConfig, {
     devServer: {
       port: 8083,
     },
+    plugins: [
+      new Dotenv({
+        path: envFilePath,
+      })
+    ],
     module: {
       rules: [
         {
