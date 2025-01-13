fe757d61aa04c13b0048f60b1578ebf95807219c
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-01-13 11:08:38 -0300

    🔧 add DefinePlugin to webpack configuration for environment variable management

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 const Dotenv = require("dotenv-webpack");
+const webpack = require('webpack');
 
 module.exports = (webpackConfigEnv, argv) => {
   const isProduction = argv.mode === "production";
@@ -27,6 +28,12 @@ module.exports = (webpackConfigEnv, argv) => {
     plugins: [
       new Dotenv({
         path: isProduction ? "./.env.production" : "./.env.development",
+      }),
+      new webpack.DefinePlugin({
+        "process.env": JSON.stringify({
+          NODE_ENV: isProduction ? "production" : "development",
+          ...process.env,
+        }),
       }),
     ],
   });
